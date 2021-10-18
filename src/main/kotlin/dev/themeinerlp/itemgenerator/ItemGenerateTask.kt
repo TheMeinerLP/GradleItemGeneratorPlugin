34ddae300b86eaad36e92ef2ebd3d316cef25e46
Phillipp Glanz <p.glanz@madfix.me>
2021-10-18 15:02:00 +0200

    Improve code generation

--- src/main/kotlin/dev/themeinerlp/itemgenerator/ItemGenerateTask.kt
+++ src/main/kotlin/dev/themeinerlp/itemgenerator/ItemGenerateTask.kt
@@ -36,39 +36,16 @@ open class ItemGenerateTask : DefaultTask() {
             .map { Json.decodeFromString<Item>(it.readText()) }
             .toSet()
         items.forEach {
-
-            /*ItemStack.builder(Material.AIR).lore(it.lore.map { s ->
-                Component.text(s)
-            }.toList())
-            list*/
-            val arrayList = ClassName("kotlin.collections", "ArrayList")
-            val component = ClassName("net.kyori.adventure.text", "Component")
-            val arrayListOfLores = arrayList.parameterizedBy(component)
-
-            val block = CodeBlock.builder()
-            it.lore.forEach {
-                block.addStatement("lore += %T.text(%S)", Component::class, it)
-            }
-            val buildItemFun = FunSpec.builder("buildItem")
+            val buildItemFun = FunSpec.builder("getItem")
                 .returns(ItemStack::class)
-                .addStatement("val lore = %T()", arrayListOfLores)
-                .addCode(block.build())
-                .addStatement("""val stack = %T.builder(%T.fromNamespaceId(%S) ?: throw %T("Material not found"))
-.amount(%L)
-.displayName(%T.text(%S))
-.lore(lore)
-.build()
-                    """,
-                    ItemStack::class,
-                    Material::class,
-                    it.material,
-                    NullPointerException::class,
-                    it.amount,
-                    Component::class,
-                    it.name)
-                .addStatement("return stack")
+                .addStatement("return this.stack")
                 .build()
             val itemTypeSpec = TypeSpec.classBuilder(it.className)
+                .addProperty(buildName(it))
+                .addProperty(buildLore(it))
+                .addProperty(buildMaterial(it))
+                .addProperty(buildAmount(it))
+                .addProperty(buildItemStack())
                 .addFunction(buildItemFun)
                 .build()
             val kotlinFile = FileSpec.builder(it.packageName, it.fileName)
@@ -76,5 +53,46 @@ open class ItemGenerateTask : DefaultTask() {
                 .build()
             kotlinFile.writeTo(getOuputDir().toPath())
         }
+    }
+
+    private fun buildItemStack(): PropertySpec {
+        val property = PropertySpec.builder("stack", ItemStack::class, KModifier.PRIVATE)
+        property.initializer("%T.builder(this.material).amount(this.amount).displayName(this.name).lore(this.lore).build()",
+            ItemStack::class)
+        return property.build()
+    }
+
+    private fun buildAmount(item: Item): PropertySpec {
+        val property = PropertySpec.builder("amount", Int::class, KModifier.PRIVATE)
+        property.initializer("%L", item.amount)
+        return property.build()
+    }
+
+    private fun buildMaterial(item: Item): PropertySpec {
+        val property = PropertySpec.builder("material", Material::class, KModifier.PRIVATE)
+        property.initializer("%T.fromNamespaceId(%S) ?: throw %T(\"Material not found\")", Material::class, item.material, NullPointerException::class)
+        return property.build()
+    }
+
+    private fun buildName(item: Item): PropertySpec {
+        val property = PropertySpec.builder("name", Component::class, KModifier.PRIVATE)
+        property.initializer("%T.text(%S)", Component::class, item.name)
+        return property.build()
+    }
+
+    private fun buildLore(item: Item): PropertySpec {
+        val block = CodeBlock.builder()
+        block.add("listOf(")
+        item.lore.forEachIndexed { index, s ->
+            if (index == (item.lore.size-1)) {
+                block.add("%T.text(%S))\n", Component::class, s)
+            } else {
+                block.add("%T.text(%S), ", Component::class, s)
+            }
+
+        }
+        val property = PropertySpec.builder("lore", List::class.parameterizedBy(Component::class), KModifier.PRIVATE)
+        property.initializer(block.build())
+        return property.build()
     }
 }
